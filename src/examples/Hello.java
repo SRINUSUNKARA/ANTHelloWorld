7c419042714e483f5a4667788cccbc8e49264ab7
SRINUSUNKARA <srenu.satyavani@gmail.com>
2017-07-29 22:09:49 +0530

    second commit

--- src/examples/Hello.java
+++ src/examples/Hello.java
@@ -43,7 +43,7 @@ public final class Hello extends HttpServlet {
         writer.println("<img src=\"images/springsource.png\">");
         writer.println("</td>");
         writer.println("<td>");
-        writer.println("<h1>Golden Technologies Welcoming you to careers</h1>");
+        writer.println("<h1>Golden Technologies Welcoming you to career and life</h1>");
         writer.println("</td>");
         writer.println("</tr>");
         writer.println("</table>");
